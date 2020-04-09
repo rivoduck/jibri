db348bbb3108a61109b70fdbc4b5c55c0737926b
Andrea Rivetti <andrea.rivetti@top-ix.org>
2020-04-09 23:03:31 +0200

    fixed bug initializing StreamingJibriService

--- src/main/kotlin/org/jitsi/jibri/service/impl/StreamingJibriService.kt
+++ src/main/kotlin/org/jitsi/jibri/service/impl/StreamingJibriService.kt
@@ -76,7 +76,7 @@ class StreamingJibriService(
     private val jibriSelenium = JibriSelenium()
 
     init {
-		var callName = streamingParams.callUrlInfo.callParams.callName;
+		var callName = streamingParams.callParams.callUrlInfo.callName;
 		var rtmpIngestionBaseUrl = "";
 		var rtmpIngestionEndpoint = "";
 		var defaultRtmpIngestionBaseUrl = "";
